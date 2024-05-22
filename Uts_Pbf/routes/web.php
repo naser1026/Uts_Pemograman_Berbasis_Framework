a87cba3cba5c70fea5963eda402bef0ebe6cc8a6
Naser Setiawan <naser.setiawan712@gmail.com>
2024-05-22 15:25:21 +0700

    Final Commit

--- Uts_Pbf/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Laravel\Socialite\Facades\Socialite;
-
-Route::get('oauth/register', function(){
-    return Socialite::driver('google')->redirect();
-});
